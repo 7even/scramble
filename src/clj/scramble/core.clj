a1b9caf04aaa04ce542e519ca2e5b4e72598d096
Vsevolod Romashov <7@7vn.ru>
2023-05-01 18:19:12 +0400

    add the POST /api/scramble endpoint

--- src/clj/scramble/core.clj
+++ src/clj/scramble/core.clj
@@ -1,9 +1,33 @@
 (ns scramble.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]))
+  (:require [bidi.ring :refer [make-handler]]
+            [clojure.spec.alpha :as s]
+            [muuntaja.middleware :as m]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [scramble.scramble :refer [scramble?]]))
 
-(defn handler [request]
-  {:status 200
-   :body "OK"})
+(s/def :scramble/str1 string?)
+(s/def :scramble/str2 string?)
+
+(s/def ::scramble
+  (s/keys :req [:scramble/str1 :scramble/str2]))
+
+(defn- scramble-handler [{params :body-params}]
+  (if (s/valid? ::scramble params)
+    {:status 200
+     :body {:scramble/success? (scramble? (:scramble/str1 params)
+                                          (:scramble/str2 params))}}
+    {:status 422
+     :body {:scramble/error "Invalid params"}}))
+
+(def routes
+  ["" {:post {"/api/scramble" scramble-handler}
+       true (fn [req]
+              {:status 404
+               :body "Not found"})}])
+
+(def handler
+  (-> (make-handler routes)
+      m/wrap-format))
 
 (defonce server
   (atom nil))
---
+++ test/clj/scramble/core_test.clj
@@ -0,0 +1,37 @@
+(ns scramble.core-test
+  (:require [clojure.edn :as edn]
+            [clojure.test :refer [deftest testing is]]
+            [ring.mock.request :as mock]
+            [scramble.core :as sut]))
+
+(deftest handler-test
+  (letfn [(get-response [params]
+            (let [request (-> (mock/request :post "/api/scramble")
+                              (mock/header "Accept" "application/edn")
+                              (mock/content-type "application/edn")
+                              (mock/body (pr-str params)))
+                  response (sut/handler request)]
+              (update response
+                      :body
+                      (comp edn/read-string slurp))))]
+    (testing "with a valid scramble"
+      (let [params {:scramble/str1 "foobar"
+                    :scramble/str2 "foo"}
+            response (get-response params)]
+        (is (= 200 (:status response)))
+        (is (= {:scramble/success? true}
+               (:body response)))))
+    (testing "with an invalid scramble"
+      (let [params {:scramble/str1 "foobar"
+                    :scramble/str2 "foobaz"}
+            response (get-response params)]
+        (is (= 200 (:status response)))
+        (is (= {:scramble/success? false}
+               (:body response)))))
+    (testing "with invalid params"
+      (let [params {:scramble/str1 :foo
+                    :scramble/str3 4}
+            response (get-response params)]
+        (is (= 422 (:status response)))
+        (is (= {:scramble/error "Invalid params"}
+               (:body response)))))))
