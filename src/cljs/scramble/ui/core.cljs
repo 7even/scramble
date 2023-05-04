0c069c1ac2617c7e02eda6e0cdb368eca69f7023
Vsevolod Romashov <7@7vn.ru>
2023-05-04 22:02:15 +0400

    make a request to server on form submit, then display the result

--- src/cljs/scramble/ui/core.cljs
+++ src/cljs/scramble/ui/core.cljs
@@ -1,20 +1,29 @@
 (ns scramble.ui.core
-  (:require [reagent.core :as r]
+  (:require [ajax.core :refer [POST]]
+            [ajax.edn :refer [edn-request-format edn-response-format]]
+            [reagent.core :as r]
             [reagent.dom :as rd]))
 
 (defonce field-values
-  (r/atom {:str1 ""
-           :str2 ""}))
+  (r/atom {:scramble/str1 ""
+           :scramble/str2 ""}))
+
+(defonce result
+  (r/atom nil))
 
 (defn- submit []
-  (println "values:" (pr-str @field-values)))
+  (POST "/api/scramble"
+        {:params @field-values
+         :format (edn-request-format)
+         :response-format (edn-response-format)
+         :handler (fn [{:scramble/keys [success?]}]
+                    (reset! result success?))}))
 
 (defn- str-input [state-key placeholder]
   [:input {:type :text
            :placeholder placeholder
            :value (get @field-values state-key)
-           :on-change #(swap! field-values assoc state-key (-> % .-target .-value))
-           :style {:width "300px"}}])
+           :on-change #(swap! field-values assoc state-key (-> % .-target .-value))}])
 
 (defn interface []
   [:div {:style {:display :flex
@@ -23,18 +32,26 @@
                  :width "100wh"
                  :height "100vh"
                  :font-size "14pt"}}
-   [:form {:style {:display :flex
+   [:form {:style {:width "300px"
+                   :display :flex
                    :flex-direction :column
                    :gap "1rem"}
            :on-submit (fn [e]
                         (.preventDefault e)
                         (submit))}
-    [str-input :str1 "rekqodlw"]
-    [str-input :str2 "world"]
+    [:div "This tool allows you to enter 2 words and check if a portion of characters from the first word can be rearranged to build the second word."]
+    [str-input :scramble/str1 "rekqodlw"]
+    [str-input :scramble/str2 "world"]
     [:div {:style {:display :flex
-                   :justify-content :center}}
+                   :gap "1rem"}}
      [:button {:on-click (fn [])}
-      "Scramble"]]]])
+      "Scramble"]
+     (when-some [success? @result]
+       (if success?
+         [:div {:style {:color :green}}
+          "Match"]
+         [:div {:style {:color :red}}
+          "Mismatch"]))]]])
 
 (defn render []
   (rd/render [interface]
